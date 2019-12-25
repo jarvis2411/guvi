9954df18b24de60a38fad5a1e7115cd926e4de43
Praghadesh <54570804+jarvis2411@users.noreply.github.com>
2019-12-25 22:51:01 +0530

    Delete number of digit.py

--- loop/number of digit.py
+++
@@ -1,8 +0,0 @@
-n = int(input("Enter the number:"))
-print("The number of digits in",n,"is",len(str(n))) #using in-built function
-m = n
-c = 0
-while n> 0:
-    n = n//10
-    c += 1
-print("The number of digits in",m,"is",c) #using normal method
