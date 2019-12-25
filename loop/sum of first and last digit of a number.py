7dcb7d43cacc0c87eced695951ad71099ce16a9a
Praghadesh <54570804+jarvis2411@users.noreply.github.com>
2019-12-25 22:53:49 +0530

    Delete sum of first and last digit of a number.py

--- loop/sum of first and last digit of a number.py
+++
@@ -1,9 +0,0 @@
-n = int(input("Enter the number:"))
-m = n
-c = 0
-while n> 0:
-    n = n//10
-    c += 1
-d = c - 1    
-e =((m % 10) + (m // (10 ** d)))
-print("The sum of first digit=",(m // (10 ** d)),"and the last digit=",m % 10,"is",e)
